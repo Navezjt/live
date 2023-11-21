cdf8f0829d36fa6c2e1bcfce3afe847fbcb84b06
Joel Navez CT <110367357+Navezjt@users.noreply.github.com>
2023-11-21 18:23:45 -0500

    Update player.js

--- player/player.js
+++ player/player.js
@@ -1,4 +1,4 @@
-  var video = document.getElementById('video');
+  var video = document.getElementById('video'); 
   var vurl = location.search.split('vurl=')[1];
   var videoSrc = vurl;
   // 检查浏览器是否支持hls
